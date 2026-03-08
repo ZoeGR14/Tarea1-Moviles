765c7cd8183cf9d6500e4a87c9f05d95e923de60
Zoe González Rodríguez <zgonzalezr2001@alumno.ipn.mx>
2026-03-07 20:56:54 -0600

    Agrega diseño y funcionalidad a Botones

--- app/src/main/java/com/example/tarea1/ButtonsFragment.kt
+++ app/src/main/java/com/example/tarea1/ButtonsFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -18,4 +22,26 @@ class ButtonsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_buttons, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val btnNormal = view.findViewById<Button>(R.id.btnNormal)
+        val imgBtn = view.findViewById<ImageButton>(R.id.imgBtn)
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+
+        btnNormal.setOnClickListener {
+            Toast.makeText(requireContext(), "Has presionado un Boton Normal", Toast.LENGTH_SHORT).show()
+        }
+
+        imgBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "Has presionado un ImageButton (Icono)", Toast.LENGTH_SHORT).show()
+        }
+
+        fab.setOnClickListener {
+            Toast.makeText(requireContext(), "Has presionado un Floating Action Button", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
 }
